13e7257e2f1739741bf172ee4eb00246a7e744ff
Vincent FRICOU <vincent+github@fricouv.eu>
2018-07-03 11:43:08 +0200

    Update logs displaying

--- includes/init.sh
+++ includes/init.sh
@@ -3,4 +3,4 @@ chown -R nginx:nginx /${H5_ROOT}
 supervisord -c /usr/local/etc/supervisord.conf &
 echo "Logs will displayed in 5 seconds"
 sleep 5
-tail -f /supervisord.log /var/log/nginx/privatebin_access.log /var/log/nginx/privatebin_error.log /var/log/nginx/privatebin.access.log
+tail -f /supervisord.log /var/log/nginx/access.log /var/log/nginx/error.log /var/log/nginx/h5ai.access.log /var/log/nginx/h5ai.slow.log
