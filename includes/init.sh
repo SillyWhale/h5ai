94e2495b41ab3ff85b706b83e006da4749ac956d
Vincent FRICOU <vincent+github@fricouv.eu>
2018-07-03 11:44:56 +0200

    fix: supervisord configuration location

--- includes/init.sh
+++ includes/init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 chown -R nginx:nginx /${H5_ROOT}
-supervisord -c /usr/local/etc/supervisord.conf &
+supervisord -c /usr/local/supervisord.conf &
 echo "Logs will displayed in 5 seconds"
 sleep 5
 tail -f /supervisord.log /var/log/nginx/access.log /var/log/nginx/error.log /var/log/nginx/h5ai.access.log /var/log/nginx/h5ai.slow.log
